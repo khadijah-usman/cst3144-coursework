46c01bb123d1b53bcb63040ed6812761d036fe14
“Khadijah <dijanu04@gmail.com>
2025-11-26 05:31:27 +0400

    matcg repo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/lesson-booking-frontend/'  
+  base: '/cst3144-coursework/'   // must match repo name on GitHub
 })
